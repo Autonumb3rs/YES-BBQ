aa947de0a7329b3ecabd2c92b2db887988faa73e
Autonumb3rs <132037652+Autonumb3rs@users.noreply.github.com>
2024-05-17 14:00:44 +0800

    Add files via upload

---
+++ yes-bbq/script.js
@@ -0,0 +1,11 @@
+function home() {
+    window.location.href="index.html";
+}
+
+function menu() {
+    window.location.href="menu.html";
+}
+
+function contact(){
+    window.location.href="contacts.html";
+}
